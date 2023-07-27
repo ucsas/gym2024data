d4a56a758a1655e22ca09f42efa39a2ed8c8608d
zefangm <89998194+zefangm@users.noreply.github.com>
2023-07-27 15:51:35 -0400

    generated 2022 Senior European Championships cleandata into data_new.csv, added data manipulation codes required

--- R/manipulation.R
+++ R/manipulation.R
@@ -6,13 +6,34 @@
 ## Central American and Caribbean Games San Salvador 2023
 
 
-folder_path <- "/Users/minzefang/gym2024data/pdfs_2023/central_am"
-col_names_vt <- c("Rank", "Bib", "Name", "NOC", "vault", "D_Score", "E_Score", "Penalty", "Score")
+ca_path <- "/Users/minzefang/gym2024data/pdfs_2023/central_am"
+col_names_vt <- c("Rank", "Bib", "Name", "NOC", "vault", "E_Score","D_Score", "Penalty", "Score")
 
-ca_ls_raw <- get_gym_tables(folder_path)
-# ca_ls_raw
+ca_ls_raw <- get_gym_tables(folder_path = ca_path)
 ca_ls <- align_tables(raw_table_list = ca_ls_raw, col_names = col_names_vt)
-# ca_ls
-ca_tb <- transform_table(table_list = ca_ls)
-# ca_tb
-write_csv(ca_tb, "cleandata/central_america.csv")  
+ca_tb <- transform_table(table_list = ca_ls, 
+                         Date = "24-28 Jun 2023", 
+                         Competition = "Central American and Caribbean Games", 
+                         Location = "San Salvador, El Salvador")
+write_csv(ca_tb, "cleandata/central_america.csv")
+
+
+## 2022 Senior European Championships MUNICH (GER)
+
+eu22_path <- "/Users/minzefang/gym2024data/pdfs_2023/europe_22"
+eu_ls_raw <- get_gym_tables(eu22_path)
+# map(eu_ls_raw, ~ map(., ncol))
+eu_ls <- align_tables(eu_ls_raw, col_names_vt)
+# map(eu_ls, ncol)
+eu_tb <- transform_table(table_list = eu_ls, 
+                         Date = "Aug 2022", 
+                         Competition = "2022 Senior European Championships ", 
+                         Location = "Munich, Germany") %>% 
+  mutate(Date = case_when(
+    Gender == "m" ~ "18-21 Aug 2022",
+    Gender == "w" ~ "11-14 Aug 2022",
+    TRUE ~ Date
+  ))
+write_csv(eu_tb, "cleandata/european_2022.csv")
+
+str(eu_tb)
