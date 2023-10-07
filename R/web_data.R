e6d6bf080d4a8a6d7d2d24a09f38ceff93f9c231
zefangm <89998194+zefangm@users.noreply.github.com>
2023-10-06 20:31:28 -0400

    added cleandata for 2022 Cairo World Cup

--- R/web_data.R
+++ R/web_data.R
@@ -8,8 +8,8 @@ url2 <- "https://thegymter.net/2022/03/03/2022-cottbus-world-cup-results/"
 ## to replace full names in source data with abbreviations
 noc_key <- read.csv("noc_key.csv")
 new_rows <- data.frame(
-  Full_Name = c("Belarus*", "Norway", "Albania", "Armenia", "Czech Republic", "Great Britain", "Hong Kong", "Turkey"),
-  Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR")
+  Full_Name = c("Belarus*", "Norway", "Albania", "Armenia", "Czech Republic", "Great Britain", "Hong Kong", "Turkey", "South Korea", "Russia*"),
+  Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR", "KOR", "RUS")
 )
 noc_data <- rbind(noc_key, new_rows)
 
@@ -83,6 +83,33 @@ doha22_tb <- transform_web_tb(table_list = doha22_tb_ls,
 
 write_csv(doha22_tb, "../cleandata/data_new/doha_22.csv")
 
+
+
+### 2022 Cairo World Cup ################################################
+
+url1 <- "https://thegymter.net/2022/03/20/2022-cairo-world-cup-mens-results/"
+url2 <- "https://thegymter.net/2022/03/20/2022-cairo-world-cup-results/"
+
+## to replace full names in source data with abbreviations
+noc_key <- read.csv("noc_key.csv")
+new_rows <- data.frame(
+  Full_Name = c("Belarus*", "Norway", "Albania", "Armenia", "Czech Republic", "Great Britain", "Hong Kong", "Turkey", "South Korea", "Russia*"),
+  Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR", "KOR", "RUS")
+)
+noc_data <- rbind(noc_key, new_rows)
+
+cairo22_m <- get_web_tb(url1, gender = "m")
+cairo22_w <- get_web_tb(url2, gender = "w")
+cairo22_tb_ls <- c(cairo22_m, cairo22_w) %>% 
+  update_vt()
+cairo22_tb <- transform_web_tb(table_list = cairo22_tb_ls, 
+                             Date = "17-20 Mar 2022",
+                             Competition = "2022 Cairo World Cup",
+                             Location = "Cairo, Egypt",
+                             NOCkey = noc_data) %>% 
+  select(-Nation)
+
+write_csv(cairo22_tb, "../cleandata/data_new/Cairo_22.csv")
 
 
 
