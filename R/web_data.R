dc398ee5362b64662b6e0b76ea965714587e9f4a
zefangm <89998194+zefangm@users.noreply.github.com>
2023-10-07 13:30:52 -0400

    added cleandata for 2022 Varna World Challenge Cup

--- R/web_data.R
+++ R/web_data.R
@@ -1,10 +1,5 @@
 source("function.R")
 
-### 2022 Cottbus World Cup ################################################
-
-url1 <- "https://thegymter.net/2022/03/02/2022-cottbus-world-cup-mens-results/"
-url2 <- "https://thegymter.net/2022/03/03/2022-cottbus-world-cup-results/"
-
 ## to replace full names in source data with abbreviations
 noc_key <- read.csv("noc_key.csv")
 new_rows <- data.frame(
@@ -12,6 +7,12 @@ new_rows <- data.frame(
   Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR", "KOR", "RUS")
 )
 noc_data <- rbind(noc_key, new_rows)
+
+
+### 2022 Cottbus World Cup ################################################
+
+url1 <- "https://thegymter.net/2022/03/02/2022-cottbus-world-cup-mens-results/"
+url2 <- "https://thegymter.net/2022/03/03/2022-cottbus-world-cup-results/"
 
 cot22_m <- get_web_tb(url1, gender = "m")
 cot22_w <- get_web_tb(url2, gender = "w")
@@ -35,14 +36,6 @@ write_csv(cot22_tb, "../cleandata/data_new/cottbus_22.csv")
 url1 <- "https://thegymter.net/2022/03/05/2022-doha-world-cup-mens-results/"
 url2 <- "https://thegymter.net/2022/03/05/2022-doha-world-cup-results/"
 
-## to replace full names in source data with abbreviations
-noc_key <- read.csv("noc_key.csv")
-new_rows <- data.frame(
-  Full_Name = c("Belarus*", "Norway", "Albania", "Armenia", "Czech Republic", "Great Britain", "Hong Kong", "Turkey", "South Korea", "Russia*"),
-  Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR", "KOR", "RUS")
-)
-noc_data <- rbind(noc_key, new_rows)
-
 doha22_m <- get_web_tb(url1, gender = "m")
 doha22_w <- get_web_tb(url2, gender = "w")
 doha22_tb_ls <- c(doha22_m, doha22_w) %>% 
@@ -50,6 +43,7 @@ doha22_tb_ls <- c(doha22_m, doha22_w) %>%
 
 doha22_tb_ls <- doha22_tb_ls %>% 
   map( ~{
+    # For Rank, change "—" into NA, then make this column numeric
     .x$Rank[.x$Rank == "—"] <- NA
     .x$Rank <- as.numeric(.x$Rank)
     return(.x)
@@ -61,6 +55,7 @@ doha22_tb_ls <- doha22_tb_ls %>%
     return(.x)
   }) %>% 
   map( ~{
+    # 将E和Total列由字符串转为数值类型
     .x$E <- as.numeric(.x$E)
     .x$Total <- as.numeric(.x$Total)
     return(.x)
@@ -90,14 +85,6 @@ write_csv(doha22_tb, "../cleandata/data_new/doha_22.csv")
 url1 <- "https://thegymter.net/2022/03/20/2022-cairo-world-cup-mens-results/"
 url2 <- "https://thegymter.net/2022/03/20/2022-cairo-world-cup-results/"
 
-## to replace full names in source data with abbreviations
-noc_key <- read.csv("noc_key.csv")
-new_rows <- data.frame(
-  Full_Name = c("Belarus*", "Norway", "Albania", "Armenia", "Czech Republic", "Great Britain", "Hong Kong", "Turkey", "South Korea", "Russia*"),
-  Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR", "KOR", "RUS")
-)
-noc_data <- rbind(noc_key, new_rows)
-
 cairo22_m <- get_web_tb(url1, gender = "m")
 cairo22_w <- get_web_tb(url2, gender = "w")
 cairo22_tb_ls <- c(cairo22_m, cairo22_w) %>% 
@@ -117,14 +104,6 @@ write_csv(cairo22_tb, "../cleandata/data_new/Cairo_22.csv")
 url1 <- "https://thegymter.net/2022/04/05/2022-baku-world-cup-mens-results/"
 url2 <- "https://thegymter.net/2022/04/04/2022-baku-world-cup-results/"
 
-## to replace full names in source data with abbreviations
-noc_key <- read.csv("noc_key.csv")
-new_rows <- data.frame(
-  Full_Name = c("Belarus*", "Norway", "Albania", "Armenia", "Czech Republic", "Great Britain", "Hong Kong", "Turkey", "South Korea", "Russia*"),
-  Country_Abbr = c("BLR", "NOR", "ALB", "ARM", "CZE", "GBR", "HKG", "TUR", "KOR", "RUS")
-)
-noc_data <- rbind(noc_key, new_rows)
-
 Baku22_m <- get_web_tb(url1, gender = "m")
 Baku22_w <- get_web_tb(url2, gender = "w")
 Baku22_tb_ls <- c(Baku22_m, Baku22_w) %>% 
@@ -139,6 +118,36 @@ Baku22_tb <- transform_web_tb(table_list = Baku22_tb_ls,
 write_csv(Baku22_tb, "../cleandata/data_new/Baku_22.csv")
 
 
+
+### 2022 Varna World Cup ################################################
+
+url1 <- "https://thegymter.net/2022/05/31/2022-varna-challenge-cup-mens-results/"
+url2 <- "https://thegymter.net/2022/05/30/2022-varna-challenge-cup-results/"
+
+varna22_m <- get_web_tb(url1, gender = "m")
+varna22_w <- get_web_tb(url2, gender = "w")
+varna22_tb_ls <- c(varna22_m, varna22_w) %>% 
+  update_vt() %>% 
+  map( ~{
+    # 使用filter函数删除D列为"-"的行
+    .x <- .x %>%
+      filter(D != "—")
+    return(.x)
+  }) %>% 
+  map( ~{
+    # 将E和Total列由字符串转为数值类型
+    .x$E <- as.numeric(.x$E)
+    .x$D <- as.numeric(.x$D)
+    return(.x)
+  })
+varna22_tb <- transform_web_tb(table_list = varna22_tb_ls, 
+                              Date = "26-29 Mar 2022",
+                              Competition = "2022 Varna World Challenge Cup",
+                              Location = "Varna, Bulgaria",
+                              NOCkey = noc_data) %>% 
+  select(-Nation)
+
+write_csv(varna22_tb, "../cleandata/data_new/varna_22.csv")
 
 
 
