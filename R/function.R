2ba1cafb15f3e4c9c5194cf44b02db3b3af71269
zefangm <89998194+zefangm@users.noreply.github.com>
2023-10-30 10:19:01 -0400

    modified name extraction algorithm for web scraping data in transform_web_tb() function, added process_web_tb_ls() function

--- R/function.R
+++ R/function.R
@@ -301,6 +301,40 @@ get_web_tb <- function(url, gender) {
   return(tables)
 }
 
+# 构造process_web_tb_ls函数，输入数据框list（和性别参数），返回增加Round和Gender的数据框list
+# 是get_web_tb函数的简化修改版，去掉了直接从网页链接获取数据框列表的步骤
+# 应用于该网页上包含不止所需要的数据框的情况，需预先把那些表格从列表中删掉，
+# 参考2022 Asian championship的处理
+process_web_tb_ls <- function(tables_list, gender) {
+  if (gender == "m") {
+    round <- c(rep("final",6), rep("qual",6))
+    appar <- rep(c("FX", "PH", "SR", "VT", "PB", "HB"),2)
+  } else if (gender == "w") {
+    round <- c(rep("final",4), rep("qual",4))
+    appar <- rep(c("VT", "UB", "BB", "FX"),2)
+  } else {
+    stop("Invalid gender. Please use 'm' or 'w'.")
+  }
+  
+  ## add new column Round for each df
+  tables_list <- lapply(1:length(tables_list), function(i) {
+    tables_list[[i]]$Round <- round[i]
+    return(tables_list[[i]])
+  })
+  
+  ## add new column Apparatus for each df
+  tables <- lapply(1:length(tables_list), function(i) {
+    tables_list[[i]]$Apparatus <- appar[i]
+    return(tables_list[[i]])
+  }) %>% 
+    lapply(function(df) {
+      df$Gender <- gender
+      return(df)
+    })
+  
+  return(tables)
+}
+
 # this function is used for change "VT" into "VT1" and "VT2" by line num
 update_vt <- function(table_list) {
   table_list <- lapply(table_list, function(df) {
@@ -313,6 +347,11 @@ update_vt <- function(table_list) {
   return(table_list)
 }
 
+### 修改transform_web_tb函数的姓名提取算法
+# 中日韩越南香港台湾是姓在前，其他皆为姓在后（包括新加坡蒙古匈牙利），
+# 香港有些情况会英文名在前（当香港且姓名为四个单词，如A B C D，姓为 B，名为 C D A）
+# 例如Frankie Lee Man Hin, 应转写为 LastName: Lee, FirstName: Man Hin Frankie
+
 transform_web_tb <- function(table_list, Date, Competition, Location, NOCkey) {
   tel_tb <- list_rbind(table_list)
   merged_tb <- merge(tel_tb, NOCkey, by.x = "Nation", by.y = "Full_Name", 
@@ -323,12 +362,23 @@ transform_web_tb <- function(table_list, Date, Competition, Location, NOCkey) {
            Score = Total,
            Date = Date, Competition = Competition, Location = Location,
            Penalty = ifelse(Penalty < 0, -Penalty, Penalty)) %>% 
-    mutate(LastName = ifelse(Nation == "Hong Kong", 
-                             str_split_fixed(Athlete, " ", 2)[,1],
-                             word(Athlete, -1)),
-           FirstName = ifelse(Nation == "Hong Kong",
-                              str_split_fixed(Athlete, " ", 2)[,2],
-                              str_replace(Athlete, LastName, ""))) %>% 
+    mutate(LastName = if_else(Country %in% last_name_first_noc,
+                              str_split_fixed(Athlete, " ", 2)[,1],
+                              word(Athlete, -1)),
+           FirstName = if_else(Country %in% last_name_first_noc,
+                               str_split_fixed(Athlete, " ", 2)[,2],
+                               str_replace(Athlete, LastName, ""))) %>% 
+    mutate(
+      # 处理HKD的特殊情况
+      LastName = case_when(
+        Country == "HKG" & str_count(Athlete, boundary("word")) == 4 ~ 
+          word(Athlete, 2),
+        TRUE ~ LastName),
+      FirstName = case_when(
+        Country == "HKG" & str_count(Athlete, boundary("word")) == 4 ~ 
+          str_c(word(Athlete, 3), word(Athlete, 4), word(Athlete, 1), sep = " "),
+        TRUE ~ FirstName
+      )) %>% 
     relocate(LastName, FirstName, Gender, Country, Date, Competition, Round, 
              Location, Apparatus, Rank, D_Score, E_Score, Penalty, Score ) %>% 
     select(!Athlete:Country_Abbr)
@@ -345,6 +395,7 @@ convert_dns_or_dash_to_empty_string <- function(df) {
   )
   return(df)
 }
+
 
 
 ### Functions for Cottbus and EnBW DTB Pokal ###################################
