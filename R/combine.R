efc6787134a931be2cc0dd2f386dd7a730c7594a
zefangm <89998194+zefangm@users.noreply.github.com>
2023-10-30 12:16:29 -0400

    manually adjust for name differences in final data

--- R/combine.R
+++ R/combine.R
@@ -25,19 +25,31 @@ combined_df <- combined_df %>%
   fill(FirstName, .direction = "downup") %>% 
   mutate(Apparatus = ifelse(Apparatus == "RG", "SR", Apparatus))
 
-## manually adjust for name differences
+## 把LastName全大写，有些来自web scraping的数据LastName没有大写
 combined_df <- combined_df %>%
   mutate(LastName = str_to_upper(LastName)) %>% 
   arrange(LastName, FirstName, Competition, Apparatus)
 
-# combined_df <- combined_df %>%
-#   mutate(FirstName = ifelse(LastName == "AAS", "Fredrik", FirstName)) %>%
-#   mutate(FirstName = ifelse(LastName == "ABIYURAFI", ".", FirstName)) %>%
-#   mutate(FirstName = if_else(LastName == "ANTILA" & FirstName == "Misella Alli Helena", "Misella", FirstName)) %>%
-#   mutate(LastName = if_else(LastName == "ACHONDO", "ACHONDO ANDINO", LastName)) %>%
-#   mutate(FirstName = if_else(LastName == "ACHONDO ANDINO", "Barbara", FirstName)) %>%
-
-
+## manually adjust for name differences
+combined_df <- combined_df %>%
+  mutate(FirstName = ifelse(LastName == "AAS", "Fredrik", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "ABIYURAFI", ".", FirstName)) %>%
+  mutate(FirstName = if_else(LastName == "ANTILA" & FirstName == "Misella Alli Helena", "Misella", FirstName)) %>%
+  mutate(LastName = if_else(LastName == "ACHONDO", "ACHONDO ANDINO", LastName)) %>%
+  mutate(FirstName = if_else(LastName == "ACHONDO ANDINO", "Barbara", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "BACSKAY", "Csenge Maria", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "BESSIT", "Tamsyn", FirstName)) %>%
+  mutate(LastName = ifelse(LastName == "BROSZIO VOITIER", "BROSZIO", LastName)) %>%
+  mutate(FirstName = ifelse(LastName == "BROSZIO", "Pablo", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "CALVO MORENO JO", "Jossimar Orlando", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "CARDERO", "Sabrina", FirstName)) %>%
+  mutate(LastName = ifelse(LastName == "CARRERES MACIA", "CARRERES", LastName)) %>%
+  mutate(FirstName = ifelse(LastName == "CEMLYN JONES", "Joseph", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "BLIXT", "Landen", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "	CHRISTOPULOS", "Taylor", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "FATTA", "Addison", FirstName)) %>%
+  mutate(FirstName = ifelse(LastName == "JOHNSON"& FirstName == "Madray Emmanuelle", "Madray", FirstName))
+  
 
 ## Write to a new csv file
 write_csv(combined_df, file.path(path_to_dir, "data_2022_2023.csv"), na = "")
